70d76a190ae06e0cc7c8eb9185703247e606537f
Konyee11 <konyee.527@gmail.com>
2024-11-30 06:20:25 +0900

    モバイルメニューを開いた時にカバーアニメーションをつける

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const cb = function (el, isIntersecting) {
-    if (isIntersecting) {
-      el.classList.add("inview");
-    } else {
-      el.classList.remove("inview");
-    }
-  };
-  const so = new ScrollObserver(".cover-slide", cb);
+    const cb = function (el, isIntersecting) {
+        if (isIntersecting) {
+            el.classList.add("inview");
+        } else {
+            el.classList.remove("inview");
+        }
+    };
+    const so = new ScrollObserver(".cover-slide", cb);
 });
