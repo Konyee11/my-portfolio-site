a29340fc4a01f9bce5ca6478846406531d5a5689
Konyee11 <konyee.527@gmail.com>
2024-11-30 09:19:17 +0900

    画面に表示されたら文字がスライドされて現れるアニメーションを作成

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const cb = function (el, isIntersecting) {
+    const _inviewAnimation = (el, isIntersecting) => {
         if (isIntersecting) {
             el.classList.add("inview");
         } else {
             el.classList.remove("inview");
         }
     };
-    const so = new ScrollObserver(".cover-slide", cb);
+    const so = new ScrollObserver(".cover-slide", _inviewAnimation);
+    const so2 = new ScrollObserver(".appear", _inviewAnimation);
 });
